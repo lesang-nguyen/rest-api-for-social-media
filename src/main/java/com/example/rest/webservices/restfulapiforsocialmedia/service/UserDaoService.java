7aa257703cf13825b14b2355662d90e50f44a470
shaun.nguyen <shaun.nguyen@ticca.com>
2025-03-25 09:58:03 +0700

    first commit

---
+++ src/main/java/com/example/rest/webservices/restfulapiforsocialmedia/service/UserDaoService.java
@@ -0,0 +1,34 @@
+package com.example.rest.webservices.restfulapiforsocialmedia.service;
+
+import com.example.rest.webservices.restfulapiforsocialmedia.entity.UserEntity;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+     private static List<UserEntity> users = new ArrayList<>();
+     static {
+         users.add(new UserEntity(1, "Adam", LocalDate.now().minusYears(30)));
+         users.add(new UserEntity(2, "Eve", LocalDate.now().minusYears(25)));
+         users.add(new UserEntity(3, "Jim", LocalDate.now().minusYears(20)));
+     }
+
+     public List<UserEntity> findAll() {
+         return users;
+     }
+
+     public UserEntity findById(Integer id) {
+         Predicate<? super UserEntity> predicate = p -> p.getId().equals(id);
+         return users.stream().filter(predicate).findFirst().orElse(null);
+     }
+
+     public void save(UserEntity user) {
+         user.setId(users.size() + 1);
+         users.add(user);
+     }
+}
